84a8a7fc15a837734cfd6dab5ca6d260bebcfbd4
MaxEaton <maximiliano.k.eaton@gmail.com>
2023-04-12 14:59:58 -0600

    Docker set up

--- src/index.js
+++ src/index.js
@@ -163,6 +163,7 @@ app.get('/welcome', (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
+// module.exports = app.listen(3000);
 app.listen(3000);
 console.log('Server is listening on port 3000');
 
